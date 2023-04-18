813db216cb84b2a01aff028c4795fa451f830a67
Partha Biswas <partha.biswas@metacube.com>
2023-04-18 16:22:54 +0530

    Date and time

---
+++ date.php
@@ -0,0 +1,11 @@
+<?php
+
+echo date("l\n");
+echo date('l jS \of F Y h:i:s A')."\n";
+echo date('h:i:s')."\n";
+echo date('H:i:s')."\n";
+echo date('Y.F.j')."\n";
+echo date('Y.M.j')."\n";
+echo date('Y.m.j')."\n";
+echo date('y.m.j')."\n";
+echo date('y.m.d')."\n";
